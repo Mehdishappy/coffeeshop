09cf2856dd586fb92c495910dba06ba898e15f9b
Mehdishappy <169170458+Mehdishappy@users.noreply.github.com>
2024-05-07 17:48:23 +0330

    test

---
+++ script.js
@@ -0,0 +1,10 @@
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    a.classList.toggle('dk-link');
+});
+// document.getElementById('footer').scrollIntoView({
+//     behavior: 'smooth'
+//   });
